cfc23f8efa1fce4766c93a66672d04e9083c3e9b
juescoryisus <125850604+juescoryisus@users.noreply.github.com>
2023-08-31 22:40:20 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listar_nombres_de_archivos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void ListFilesRecursively(string directory)
+        {
+            foreach (string file in Directory.GetFiles(directory))
+            {
+                listBoxFiles.Items.Add($"Archivo: {Path.GetFileName(file)}, Extensión: {Path.GetExtension(file)}");
+                listBoxFiles.Items.Add("---------------------------------------------------------------------------------------------------");
+            }
+
+            foreach (string subdirectory in Directory.GetDirectories(directory))
+            {
+                ListFilesRecursively(subdirectory);
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBoxFiles.Items.Clear();
+            string path = @"C:\Users\jesus\OneDrive\Escritorio\MUSICA"; // Cambia esto a la ruta de tu directorio raíz
+            ListFilesRecursively(path);
+        }
+    }
+}
